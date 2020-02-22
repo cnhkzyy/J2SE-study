5382f26f6b4a7c1604339fba5c331f31cd900d92
cnhkzyy <1069966476@qq.com>
2020-02-22 18:21:51 +0800

    更新TestSync方法

--- src/main/java/com/chapter9/TestSync.java
+++ src/main/java/com/chapter9/TestSync.java
@@ -26,13 +26,30 @@ public class TestSync implements Runnable{  //自身是一个线程类
 }
 
 
+//class Timer1 {
+//    private static int num = 0;
+//    public void add(String name) {
+//        num ++;
+//        try {
+//            Thread.sleep(1);    //t1睡了1s被t2打断，num由1变为2
+//        } catch (InterruptedException e) {}
+//        System.out.println(name + ", 你是第" + num + "个使用timer的线程");
+//    }
+//}
+
+
+
 class Timer1 {
     private static int num = 0;
-    public void add(String name) {
-        num ++;
-        try {
-            Thread.sleep(1);
-        } catch (InterruptedException e) {}
-        System.out.println(name + ", 你是第" + num + "个使用timer的线程");
+
+    public synchronized void add(String name) {    //另一种简便的写法，在执行方法的过程中锁定当前对象，锁定this
+        //synchronized (this) {   //synchronized 锁定当前对象，一个线程执行的过程中，不会被另一个线程打断
+            num++;   //成员变量num也被锁定，互斥锁
+            try {
+                Thread.sleep(1);    //t1睡了1s被t2打断，num由1变为2
+            } catch (InterruptedException e) {
+            }
+            System.out.println(name + ", 你是第" + num + "个使用timer的线程");
+        //}
     }
 }
